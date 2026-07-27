40e7414b058bb250937627a53a1795cba25ea795
krishan1myadav <krishna1myadav.@gmail.com>
2026-07-27 20:50:13 +0530

    today we upload Driver Class

---
+++ src/BankingManagementSystem/BankingApp.java
@@ -0,0 +1,16 @@
+package BankingManagementSystem;
+
+import java.sql.*;
+import java.util.Scanner;
+
+import static java.lang.Class.forName;
+
+public class BankingApp {
+    private static final String url = "jdbc:mysql://localhost:3306/banking_system";
+    private static final String username = "root";
+    private static final String password = "66669999";
+
+    public static void main( String[] args )throws ClassNotFoundException, SQLException{
+
+    }
+}
