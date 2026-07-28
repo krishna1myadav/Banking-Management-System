ef51bfe15393d432aa1d6b3a3a2881fd73d359e6
krishan1myadav <krishna1myadav.@gmail.com>
2026-07-28 19:33:59 +0530

    connection established with DataBase

--- src/BankingManagementSystem/BankingApp.java
+++ src/BankingManagementSystem/BankingApp.java
@@ -11,6 +11,24 @@ public class BankingApp {
     private static final String password = "66669999";
 
     public static void main( String[] args )throws ClassNotFoundException, SQLException{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch( ClassNotFoundException e){
+            System.out.println( e.getMessage() );
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Scanner scanner = new Scanner(System.in);
+
+            User user = new User( connection, scanner );
+            Accounts accounts = new Accounts( connection, scanner );
+            AccountManager accountManager = new AccountManager( connection, scanner );
+
+            String email ;
+            long account_number;
+
+        }
 
     }
 }
