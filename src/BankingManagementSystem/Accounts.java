80bcc3ce5481c4ddd35e388c5431386e307a836e
krishan1myadav <krishna1myadav.@gmail.com>
2026-07-31 23:23:04 +0530

    continue with account

--- src/BankingManagementSystem/Accounts.java
+++ src/BankingManagementSystem/Accounts.java
@@ -12,4 +12,26 @@ public class Accounts {
         this.scanner = scanner;
     }
 
+    public long open_account(String email){
+        if(!account_exist(email)){
+            Sting open_account_query = "INSERT INTO Acounts(account_number, full_name, email, balance, security_pin) VALUES(?, ?, ?, ?, ?)";
+            scanner.nextLine();
+            System.out.println("Enter Full Name: ");
+            String full_name = scanner.nextLine();
+            System.out.println("Enter Initial Account: ");
+            double balance = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("Enter Security Pin:");
+            String security_pin = scanner.nextLine();
+            try{
+                long account_number = generateAccountNumber();
+                PreparedStatement preparedStatement = connection.prepareStatement(open_account_query);
+                preparedStatement.setLog(1, account_number);
+                preparedStatement.setString(2, full_name);
+                preparedStatement.setString(3, email);
+                preparedStatement.setString(4, balance);
+            }
+        }
+    }
+
 }
