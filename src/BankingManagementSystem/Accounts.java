0f46b958067d24ae72d40156846b1a1018fd1fb5
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-02 00:07:03 +0530

    Account Complete

--- src/BankingManagementSystem/Accounts.java
+++ src/BankingManagementSystem/Accounts.java
@@ -14,7 +14,7 @@ public class Accounts {
 
     public long open_account(String email){
         if(!account_exist(email)){
-            Sting open_account_query = "INSERT INTO Acounts(account_number, full_name, email, balance, security_pin) VALUES(?, ?, ?, ?, ?)";
+            String open_account_query = "INSERT INTO Acounts(account_number, full_name, email, balance, security_pin) VALUES(?, ?, ?, ?, ?)";
             scanner.nextLine();
             System.out.println("Enter Full Name: ");
             String full_name = scanner.nextLine();
@@ -26,11 +26,11 @@ public class Accounts {
             try{
                 long account_number = generateAccountNumber();
                 PreparedStatement preparedStatement = connection.prepareStatement(open_account_query);
-                preparedStatement.setLog(1, account_number);
+                preparedStatement.setLong(1, account_number);
                 preparedStatement.setString(2, full_name);
                 preparedStatement.setString(3, email);
-                preparedStatement.setString(4, balance);
-                preparedStatement.setDouble(5, security_pin);
+                preparedStatement.setDouble(4, balance);
+                preparedStatement.setString(5, security_pin);
                 int rowsAffected = preparedStatement.executeUpdate();
                 if(rowsAffected > 0){
                     return account_number;
@@ -45,6 +45,18 @@ public class Accounts {
     }
 
     public long getAccount_number(String email){
+        String query = "SELECT account_number from Accounts WHERE email = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return resultSet.getLong("account_number");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        throw new RuntimeException("Account Number Does Not Exist!!");
 
     }
 
