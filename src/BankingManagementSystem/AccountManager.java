7464007094eeebfcdc5a29dd075b907197e1a23e
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-04 23:26:57 +0530

    continue

--- src/BankingManagementSystem/AccountManager.java
+++ src/BankingManagementSystem/AccountManager.java
@@ -2,6 +2,7 @@ package BankingManagementSystem;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -20,12 +21,25 @@ public class AccountManager {
         double amount = scanner.nextDouble();
         scanner.nextLine();
         System.out.print("Enter Security Pin: ");
-        String securityPin = scanner.nextLine();
+        String security_pin = scanner.nextLine();
 
         try{
             connection.setAutoCommit(false);
             if(account_number != 0){
                 PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Accounts WHERE security_pin = ? ");
+                preparedStatement.setString(1, security_pin);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if(resultSet.next()){
+                    String credit_query = "UPDATE Accounts SET balance = balance + ? WHERE account_number = ?";
+                    PreparedStatement preparedStatement1 = connection.prepareStatement(credit_query);
+                    preparedStatement1.setDouble(1, amount);
+                    preparedStatement1.setLong(2, account_number);
+                    int rowsAffected = preparedStatement1.executeUpdate();
+                    if( rowsAffected > 0){
+
+                    }
+                }
 
             }
         }
